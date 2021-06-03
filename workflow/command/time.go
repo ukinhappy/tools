b90cfc888ead2a4e5b444a1acfbdc2beec8f3943
caigaoxing <caigaoxing@douyu.tv>
2021-06-03 17:01:21 +0800

    fix

--- workflow/command/time.go
+++ workflow/command/time.go
@@ -15,25 +15,33 @@ var timeCmd = &cobra.Command{
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		var flowresult XmlItems
-		var result string
-
+		var result1, result2 string
 		if len(args) > 0 {
 			ts, _ := strconv.Atoi(args[0])
 			if ts > 0 {
-				result = timex.Unix(int64(ts)).String()
+				result1 = timex.Unix(int64(ts)).String()
+				result2 = strconv.Itoa(ts)
 			} else {
-				result = strconv.Itoa(int(timex.String(args[0] + " " + args[1]).UnixSecond()))
+				result1 = strconv.Itoa(int(timex.String(args[0] + " " + args[1]).UnixSecond()))
+				result2 = args[0] + " " + args[1]
 			}
-
 		} else {
-			result = strconv.Itoa(int(timex.Now().UnixSecond()))
+			now := timex.Now().UnixSecond()
+			result1 = timex.Unix(now).String()
+			result2 = strconv.Itoa(int(timex.String(result1).UnixSecond()))
 		}
 		flowresult.Iterm = append(flowresult.Iterm,
 			Item{
-				Title:    result,
-				Subtitle: "时间点:" + result,
-				Arg:      result,
-				Valid:    "yes"})
+				Title:    result2,
+				Subtitle: "时间点:" + result2,
+				Arg:      result2,
+				Valid:    "yes"},
+			Item{
+				Title:    result1,
+				Subtitle: "时间点:" + result1,
+				Arg:      result1,
+				Valid:    "yes"},
+		)
 		b, _ := xml.Marshal(flowresult)
 		fmt.Println(string(b))
 	},
